daf1e08ea232ae132bcff5d23f8dc652295abd45
gustafssonjoel <joel.gustafsson.a@gmail.com>
2026-07-02 19:16:57 +0300

    Added scaffolded watcher

---
+++ include/sftp_watcher.h
@@ -0,0 +1,9 @@
+#ifndef SFTP_WATCHER_H
+#define SFTP_WATCHER_H
+
+#include "app_error.h"
+#include "config.h"
+
+app_error_t sftp_watcher_run(const app_config_t *config);
+
+#endif
---
+++ src/sftp_watcher.c
@@ -0,0 +1,6 @@
+#include "sftp_watcher.h"
+
+app_error_t sftp_watcher_run(const app_config_t *config) {
+    (void)config;
+    return APP_ERR_NOT_IMPLEMENTED;
+}
