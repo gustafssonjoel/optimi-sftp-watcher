d8bb0058655b47169fe379692f0407010c14ef69
gustafssonjoel <joel.gustafsson.a@gmail.com>
2026-07-02 19:01:17 +0300

    Added the scaffolded main source

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "app_error.h"
+#include "config.h"
+#include "sftp_watcher.h"
+
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+
+    fputs("optimi-sftp-watcher scaffold only; implementation pending", stderr);
+    return APP_ERR_NOT_IMPLEMENTED;
+}
